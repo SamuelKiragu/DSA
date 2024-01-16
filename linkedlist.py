c1d7bcd22d1dbed9e8bc9c9aad3d539246810906
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-16 14:38:12 +0300

    - Update - Class SinglyLinkedList - Added Iteration function

--- linkedlist.py
+++ linkedlist.py
@@ -24,4 +24,12 @@ class SinglyLinkedList:
         obsolete_node = lst.first_node
         lst.fist_node = obsolete_node.next
 
+    def iteration(self, some_node):
+        if some_node != None:
+            node = some_node
+        while True
+            # TODO:
+            node = node.next
+            if node != some_node: break
+
 
