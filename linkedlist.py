06b06464d51cc2f0400bbac88516a82197b6639a
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-17 09:39:34 +0300

    Refactored code Used Sentinel nodes in the list structure Removed special case singly linear list functions namely: - insert_beginning - remove_beginning

--- linkedlist.py
+++ linkedlist.py
@@ -11,30 +11,22 @@ class DoublyNode:
 
 # Implementation of a singly linear linked list
 class SinglyLinkedList:
-    def __init__(self, first_node):
-        self.first_node = first_node
+    def __init__(self, first_node, last_node):
+        self.first_node = SinglyNode(None,None)
     
     def insert_after(self, node, new_node):
         new_node.next = node.next
         node.next = new_node
 
-    def insert_beginning(self, lst, new_node):
-        new_node.next = lst.first_node
-        lst.first_node = new_node
-
     def remove_after(self, node):
         obsolete_node = node.next
         node.next = obsolete_node.next
-
-    def remove_beginning(self, lst):
-        obsolete_node = lst.first_node
-        lst.fist_node = obsolete_node.next
+        # TODO: Destroy obsolete node
 
     def iteration(self, some_node):
         if some_node != None:
             node = some_node
         while True
-            # TODO:
             node = node.next
             if node != some_node: break
 
