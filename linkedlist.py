028ad3281d3b1cc6f2596e81893020765c63b6fb
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-18 10:55:33 +0300

    Refactored code - CircularlySinglyLinkedList

--- linkedlist.py
+++ linkedlist.py
@@ -3,21 +3,22 @@ class SinglyNode:
         self.data = data
         self.next = next
 
-        print(f'Created Node with data value: {self.data}')
+    def __repr__(self) -> str:
+        return f'Singly Node(data={self.data})'
 
 class Node:
     def __init__(self, data, next=None, prev=None):
         self.data = data
-        self.nxt = next
+        self.next = next
         self.prev = prev
     
     def __repr__(self) -> str:
-        return f'Node(data={self.data})'
+        return f'Doubly Node(data={self.data})'
 
 # Implementation of a singly linear linked list
 class SinglyLinkedList:
     def __init__(self):
-        self.first_node = SinglyNode(None,None)
+        self.first_node = SinglyNode(None)
     
     def insert_after(self, node, new_node):
         new_node.next = node.next
@@ -30,31 +31,21 @@ class SinglyLinkedList:
         node.next = obsolete_node.next
 
         print(f'Removed node {node}')
-        # TODO: Destroy obsolete node
 
 # Implementation of a Circularly singly linked list
 class CircularlySinglyLinkedList:
     def __init__(self):
-        self.last_node = SinglyLinkedList()
+        self.__sentinel = SinglyNode(data=None)
+        self.__sentinel.next = self.__sentinel
 
     def insert_after(self, node, new_node):
-        if node == None:
-            new_node.next = new_node
-        else:
-            new_node.next = node.next
-            node.next = new_node
-
-        if self.last_node == node:
-            self.last_node = new_node
+        new_node.next = node.next
+        node.next = new_node
 
     def insert_before(self, node, new_val):
-        if node == None:
-            new_node = SinglyNode(new_val, None)
-            new_node.next = new_node
-        else:
-            new_node = SinglyNode(node.data, node.next)
-            node.data = new_val
-            node.next = new_node
+        new_node = SinglyNode(node.data, node.next)
+        node.data = new_val
+        node.next = new_node
 
     def remove(self, node):
         if node != None and len(self) > 1:
