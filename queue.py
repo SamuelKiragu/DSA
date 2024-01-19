7e7ad7aa368d512423ef9c58b21255f3816104f0
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-19 11:39:18 +0300

    Defined the functions for the queue

--- queue.py
+++ queue.py
@@ -1,9 +1,12 @@
 class Queue:
-    def __init__():
-        pass
+    def __init__(self):
+        self.items = [] # Using a dynamic array
 
-    def enqueue(element):
-        pass
+    def enqueue(self, element):
+        self.items.append(element)
+        return element # Return the added element
 
-    def dequeue(element):
-        pass
+    def dequeue(self, element):
+        item = self.items.pop(0)
+        return item # Return the removed element
+
