7043c2c629f52a56e7b476c632883f13ea57587e
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-19 11:48:22 +0300

    Declared Class Queue1 and Queue2

--- queue.py
+++ queue.py
@@ -1,6 +1,7 @@
+# Using Dynamic Arrays
 class Queue:
     def __init__(self):
-        self.items = [] # Using a dynamic array
+        self.items = []
 
     def enqueue(self, element):
         self.items.append(element)
@@ -10,3 +11,24 @@ class Queue:
         item = self.items.pop(0)
         return item # Return the removed element
 
+# Using Singly Linked Lists
+class Queue1:
+    def __init__(self):
+        pass
+
+    def enqueue(self, element):
+        pass
+
+    def dequeue(self, element):
+        pass
+
+# Using Doubly Linked Lists
+class Queue2:
+    def __init__(self):
+        pass
+
+    def enqueue(self, element):
+        pass
+    
+    def dequeue(self, element):
+        pass
