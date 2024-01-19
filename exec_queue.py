1144f8ea40036e1ecb74e88851737cf9bd47e29f
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-19 16:15:19 +0300

    Added tests for queue implemented using singly linked list

--- exec_queue.py
+++ exec_queue.py
@@ -3,6 +3,7 @@ import linkedlist as ll
 
 # Create queue
 my_queue = q.Queue()
+my_queue1 = q.Queue1()
 
 # Create nodes
 node1 = ll.Node(21)
@@ -10,13 +11,22 @@ node2 = ll.Node(223)
 node3 = ll.Node(50)
 node4 = ll.Node(1)
 
-# Add nodes to queue
+# Add nodes to my_queue
 my_queue.enqueue(node1)
 my_queue.enqueue(node2)
 my_queue.enqueue(node3)
 my_queue.enqueue(node4)
 
-# Remove nodes from queue
+
+# Add noteds to my_queue1
+my_queue1.enqueue(node3)
+my_queue1.enqueue(node1)
+my_queue1.enqueue(node4)
+
+# Remove nodes from my_queue
 print(my_queue.dequeue())
 print(my_queue.dequeue())
 
+
+# Remove nodes from my_queue
+print(my_queue1.dequeue())
